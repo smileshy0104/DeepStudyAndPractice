48c8cebf90bbccecbadcab19390faba255c6998e
yuyansong <1259581033@qq.com>
2025-07-18 20:04:32 +0800

    feat: 添加逻辑回归实战-乳腺癌实战脚本
    - 新增 Python 脚本文件，用于逻辑回归在乳腺癌数据集上的实战
    - 导入必要的库和模块，包括 numpy、pandas、scikit-learn 等
    - 准备进行数据预处理、模型训练和评估

---
+++ 逻辑回归实战-乳腺癌实战.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from  sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import classification_report
+
